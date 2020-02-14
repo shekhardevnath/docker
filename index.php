bc2b5679844939de6c808f48bb5e0c5c69bd2a56
Shekhar Devnath <shekhar@solarreviews.com>
2020-02-14 13:54:23 +1030

    Add php support for nginx

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <title>This is the title of the webpage!</title>
+  </head>
+  <body>
+    <?php
+      echo "<h1>Welcome!!</h1>"
+    ?>
+    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
+  </body>
+</html>
